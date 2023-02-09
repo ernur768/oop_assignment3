f502cf1d7c96d9ce3527db103dfa538f1196f804
Yernur <220776@astanait.edu.kz>
2023-02-09 13:37:30 +0600

    Product
    * deleted ExpirationDate field

--- src/Product.java
+++ src/Product.java
@@ -3,15 +3,13 @@ public class Product {
     private int id;
     private String name;
     private int price;
-    private String expirationDate;
     private int remained;
     private boolean isAvailable;
 
-    public Product(int id, String name, int price, String expiration_date, int remained){
+    public Product(int id, String name, int price, int remained){
         setId(id);
         setName(name);
         setPrice(price);
-        setExpirationDate(expiration_date);
         setRemained(remained);
     }
 
@@ -28,9 +26,6 @@ public class Product {
         this.price = price;
     }
 
-    public void setExpirationDate(String expirationDate) {
-        this.expirationDate = expirationDate;
-    }
 
     public void setRemained(int remained) {
         this.remained = remained;
@@ -46,10 +41,6 @@ public class Product {
 
     public int getPrice() {
         return price;
-    }
-
-    public String getExpirationDate() {
-        return expirationDate;
     }
 
     public int getRemained() {
