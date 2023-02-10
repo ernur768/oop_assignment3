e76d74e5e22dc49e85154bedde4cbb307a3ae9f6
Yernur <220776@astanait.edu.kz>
2023-02-10 12:19:33 +0600

    ProductRepository
    * added buyProduct method

--- src/repositories/interfaces/IProductRepository.java
+++ src/repositories/interfaces/IProductRepository.java
@@ -5,7 +5,7 @@ import entities.Product;
 public interface IProductRepository {
 
     Product findProduct(String productName);
-    boolean buyProduct(Product product);
+    boolean buyProduct(Product product, int quantity);
 
 
 }
